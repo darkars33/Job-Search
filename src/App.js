343163c0c09f090d5b4b363c761f12ae64679846
darkars33 <darshangarad18@gmail.com>
2023-11-23 01:11:25 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import img from "./Assets/images/account.svg"
+import Data from "./Data";
+import headerdesktop from './Assets/images/bg-header-desktop.svg';
+import account from './Assets/images/account.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="header-img">
+        <img src={headerdesktop} alt="" style={{width:'100%', height:'100%'}}/>
+      </div>
+      {Data.map((items) =>{
+        const {company, logo, position, postedAt, contract, location, languages} = items;
+        return <div className="container">
+        <div className="box">
+          <div className="img-info">
+            <div className="img">
+                <img src={logo} alt="" />
+            </div>
+            <div className="info">
+              <label style={{display:'flex', gap:'1rem', alignItems:"center"}}><h3 style={{fontWeight:"500", color:"hsl(180, 100%, 41%)"}}>{company}</h3> <button style={{padding:'0.4rem', borderRadius:'30px', border:"none", fontSize:"18px", backgroundColor:"hsl(180, 100%, 31%)", color:"#fff"}}>new!</button></label>
+              <h3 style={{color:"hsl(180, 100%, 41%)"}}>{position}</h3>
+              <ul style={{display:'flex', gap:'1.5rem', color:"rgb(150, 147, 147)"}}><p>{postedAt}</p><li>{contract}</li> <li>{location}</li></ul>
+            </div>
+          </div>
+
+          <div className="btn-container">
+            {languages.map((language) =>{
+              return(
+                <button>{language}</button>
+              )
+            })}
+          </div>
+
+        </div>
+      </div>
+      })}
+    </>
   );
 }
 
